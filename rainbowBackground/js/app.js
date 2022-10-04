84736201ed5bf02d20eb1c42afbe8162281ceee2
awizee <muhammedawell@hotmail.com>
2022-10-04 09:23:43 +0100

    initial commit

--- rainbowBackground/js/app.js
+++ rainbowBackground/js/app.js
@@ -1,1 +1,50 @@
+const rainbowColor = (color, delay, doNext) => {
+  setInterval(() => {
+    document.body.style.backgroundColor = color;
+    doNext && doNext();
+  }, delay);
+};
 
+rainbowColor("red", 1000, () => {
+  rainbowColor("orange", 1000, () => {
+    rainbowColor("yellow", 1000, () => {
+      rainbowColor("green", 1000, () => {
+        rainbowColor("blue", 1000, () => {
+          rainbowColor("indigo", 1000, () => {
+            rainbowColor("violet", 1000, () => {});
+          });
+        });
+      });
+    });
+  });
+});
+
+// const rainbowColor = (color, delay) => {
+//   new Promise((resolve, reject) => {
+//     setInterval(() => {
+//       document.body.style.backgroundColor = color;
+//       resolve();
+//     }, delay);
+//   });
+// };
+
+// rainbowColor("red", 1000)
+//   .then(() => rainbowColor("orange", 1000))
+//   .then(() => rainbowColor("yellow", 1000))
+//   .then(() => rainbowColor("green", 1000))
+//   .then(() => rainbowColor("blue", 1000))
+//   .then(() => rainbowColor("indigo", 1000))
+//   .then(() => rainbowColor("violet", 1000));
+
+// const randColor = () => {
+//  setInterval(() => {
+// 	 const r = Math.floor(Math.random() * 255);
+// 	  const g = Math.floor(Math.random() * 255);
+// 	  const b = Math.floor(Math.random() * 255);
+// 	  const a = Math.random().toFixed(1);
+
+// 	  const newColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+// 	  document.body.style.backgroundColor = newColor;
+// }, 1000);
+// };
+// randColor();
